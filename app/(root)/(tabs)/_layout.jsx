673c59354c4d510fd6b518d67c54a4533353133f
Anshul Kumar <anshulkm17@gmail.com>
2025-01-24 19:13:00 +0530

    need to fix Oauth

--- app/(root)/(tabs)/_layout.jsx
+++ app/(root)/(tabs)/_layout.jsx
@@ -3,9 +3,12 @@ import React from 'react'
 import { Tabs } from 'expo-router';
 import icons from '@/constants/icons';
 
-const TabIcon = ({ focused, icon, title}) => (
-    <View>
-        <Image source={icon} />
+const TabIcon = ({ focused, icon, title }) => (
+    <View className="flex-1 mt-3 flex flex-col items-center">
+        <Image source={icon} tintColor={focused ? '#0061ff' : '#666876'} resizeMode="contain" className="size-6" />
+        <Text className={`${focused ? 'text-primary-300 font-rubik-medium' : 'text-black-200 font-rubik'} text-xs w-full text-center mt-1`}>
+            {title}
+        </Text>
     </View>
 )
 
@@ -23,20 +26,42 @@ const TabsLayout = () => {
                 }
             }}
         >
-        <Tabs.Screen
-            name="index"
-            options={{
-                title: 'Home',
-                headerShown: false,
-                tabBarIcon: () => (
-                    <View>
-                        <TabIcon icon={icons.home} focused={focused} title={home} />
-                    </View>
-                )
-            }}
-            >
-
-            </Tabs.Screen>
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: 'Home',
+                    headerShown: false,
+                    tabBarIcon: () => (
+                        <View>
+                            <TabIcon icon={icons.home} focused={focused} title="home" />
+                        </View>
+                    )
+                }}
+            />
+            <Tabs.Screen
+                name="explore"
+                options={{
+                    title: 'Explore',
+                    headerShown: false,
+                    tabBarIcon: () => (
+                        <View>
+                            <TabIcon icon={icons.search} focused={focused} title="Explore" />
+                        </View>
+                    )
+                }}
+            />
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: 'Profile',
+                    headerShown: false,
+                    tabBarIcon: () => (
+                        <View>
+                            <TabIcon icon={icons.person} focused={focused} title="Profile" />
+                        </View>
+                    )
+                }}
+            />
         </Tabs>
     )
 }
