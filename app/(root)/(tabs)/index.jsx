e744ab913d52b2057f57ff45ed5e806c9defbd88
Anshul Kumar <anshulkm17@gmail.com>
2025-01-28 12:36:42 +0530

    home page design complete

--- app/(root)/(tabs)/index.jsx
+++ app/(root)/(tabs)/index.jsx
@@ -1,71 +1,82 @@
-import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView, FlatList } from 'react-native';
 import React from 'react';
 import { Link } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import images from '@/constants/images';
 import icons from '@/constants/icons';
 import Search from '@/components/Search';
-import { FeaturedCard } from '@/components/Cards';
-import { Card } from '@/components/Cards';
+import { Card, FeaturedCard } from '@/components/Cards';
 import Filters from '@/components/Filters';
 
+
 const Index = () => {
+
     return (
         <SafeAreaView className='bg-white h-full'>
 
-            <View className='px-5'>
-                <View className='flex flex-row items-center justify-between mt-5'>
-                    <View className='flex flex-row items-center ml-2 justify-center'>
-                        <Image source={images.avatar} className='size-12 rounded-full' />
+            <FlatList
+                data={[1, 2, 3, 4]}
+                renderItem={({ item }) => <Card />}
+                keyExtractor={(item) => item.toString()}
+                numColumns={2}
+                contentContainerClassName="pb-32"
+                columnWrapperClassName='flex gap-5 px-5'
+                showsVerticalScrollIndicator={false}
+                ListHeaderComponent={
+                    <View className='px-5'>
+                        <View className='flex flex-row items-center justify-between mt-5'>
+                            <View className='flex flex-row items-center ml-2 justify-center'>
+                                <Image source={images.avatar} className='size-12 rounded-full' />
 
-                        <View className='flex flex-col items-start ml-2 justify-center'>
-                            <Text className='text-xs font-rubik text-black-100'>
-                                Good Morning
-                            </Text>
-                            <Text className='text-base font-rubik-medium text-black-300'>
-                                User
-                            </Text>
+                                <View className='flex flex-col items-start ml-2 justify-center'>
+                                    <Text className='text-xs font-rubik text-black-100'>
+                                        Good Morning
+                                    </Text>
+                                    <Text className='text-base font-rubik-medium text-black-300'>
+                                        User
+                                    </Text>
+                                </View>
+                            </View>
+                            <Image source={icons.bell} className='size-6' />
                         </View>
-                    </View>
-                    <Image source={icons.bell} className='size-6' />
-                </View>
 
-                <Search />
-                <ScrollView vertical showVerticalScrollIndicator={false}>
-                    <View className='my-5'>
-                        <View className='flex flex-row items-center justify-between'>
-                            <Text className='text-xl font-rubik-bold text-black-300'>Featured</Text>
-                            <TouchableOpacity>
-                                <Text className='text-base font-rubik-bold text-primary-300'>See All</Text>
-                            </TouchableOpacity>
+                        <Search />
+                        <View className='my-5'>
+                            <View className='flex flex-row items-center justify-between'>
+                                <Text className='text-xl font-rubik-bold text-black-300'>Featured</Text>
+                                <TouchableOpacity>
+                                    <Text className='text-base font-rubik-bold text-primary-300'>See All</Text>
+                                </TouchableOpacity>
+                            </View>
                         </View>
-                    </View>
 
-                    <ScrollView horizontal showHorizontalScrollIndicator={false}>
-                        <View className="flex flex-row gap-x-5 mt-5 pb-3">
-                            <FeaturedCard />
-                            <FeaturedCard />
-                            <FeaturedCard />
+                        <FlatList
+                            data={[1, 2]}
+                            renderItem={({ item }) => <FeaturedCard />}
+                            keyExtractor={(item) => item.toString()}
+                            horizontal
+                            bounces={false}
+                            showsHorizontalScrollIndicator={false}
+                            contentContainerClassName='flex gap-5 px-5'
+                        />
+
+
+                        <View className='my-5'>
+                            <View className='flex flex-row items-center justify-between'>
+                                <Text className='text-xl font-rubik-bold text-black-300'>Our Recommendation</Text>
+                                <TouchableOpacity>
+                                    <Text className='text-base font-rubik-bold text-primary-300'>See All</Text>
+                                </TouchableOpacity>
+                            </View>
                         </View>
-                    </ScrollView>
 
-                    <View className='my-5'>
-                        <View className='flex flex-row items-center justify-between'>
-                            <Text className='text-xl font-rubik-bold text-black-300'>Our Recommendation</Text>
-                            <TouchableOpacity>
-                                <Text className='text-base font-rubik-bold text-primary-300'>See All</Text>
-                            </TouchableOpacity>
-                        </View>
+                        <Filters />
+
+
                     </View>
+                }
+            />
 
-                    <Filters />
-
-                    <View className='flex flex-row gap-5 mt-5'>
-                        <Card />
-                        <Card />
-                    </View>
-                </ScrollView>
-            </View>
         </SafeAreaView>
     );
 };
--- components/Filters.jsx
+++ components/Filters.jsx
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native'
 import React, { useState } from 'react'
-import { useLocalSearchParams } from 'expo-router'
+import { useLocalSearchParams, router } from 'expo-router'
 import { categories } from '@/constants/data';
 
 const Filters = () => {
@@ -8,13 +8,22 @@ const Filters = () => {
     const params = useLocalSearchParams();
     const [selectedCategory, setSelectedCategory] = useState(params.filter || 'All');
 
-    const handleCategoryPress = (category) => { }
+    const handleCategoryPress = (category) => {
+        if (selectedCategory == category) {
+            setSelectedCategory('All');
+            router.setParams({filter: 'all'});
+            return;
+        }
+
+        setSelectedCategory(category);
+        router.setParams({filter: category});
+    }
 
     return (
         <ScrollView horizontal showHorizontalScrollIndicator={false} className="mt-3 mb-2 pb-3">
             {categories.map((item, index) => (
-                <TouchableOpacity key={index} onPress={() => handleCategoryPress(item.category)} className={`flex flex-col items-start mr-4 px-4 py-2 rounded-full ${selectedCategory == item.category ? 'bg-primary-300 text-white' : 'bg-primary-100 border border-primary-200'} `}>
-                    <Text>{item.title}</Text>
+                <TouchableOpacity key={index} onPress={() => handleCategoryPress(item.category)} className={`flex flex-col items-start mr-4 px-4 py-2 rounded-full ${selectedCategory == item.category ? 'bg-primary-300' : 'bg-primary-100 border border-primary-200'} `}>
+                    <Text className={`text-sm ${selectedCategory == item.category ? 'text-white font-rubik-bold mt-0.5' : 'text-black-300 font-rubik'} `}>{item.title}</Text>
                 </TouchableOpacity>
             ))}
         </ScrollView>
